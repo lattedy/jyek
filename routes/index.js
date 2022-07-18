6f31c2ef4783373e55352f44e05ae911a1845f88
lattedy <stary7stars@naver.com>
2022-07-18 20:17:33 +0900

    aws deploy pipeline test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("aws deploy test");
   res.render('index', { title: 'Express' });
 });
 
