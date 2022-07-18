b898a670a276d819819356bf8a33ca9777276d54
lattedy <stary7stars@naver.com>
2022-07-18 23:02:02 +0900

    aws test again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("aws deploy test");
+  console.log("aws deploy test3");
   res.render('index', { title: 'Express' });
 });
 
