8622ae12784b7db2e8c188394339e38b19d315a0
lattedy <stary7stars@naver.com>
2022-07-26 21:10:28 +0900

    [front]<feat> first commit for front

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("aws deploy test3");
-  res.render('index', { title: 'Express' });
+ // res.render('index', { title: 'Express' });
+ res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 module.exports = router;
